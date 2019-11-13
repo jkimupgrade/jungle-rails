d34e36a237a765ad918edc8d596e2f33402c5deb
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-13 15:55:47 -0500

    added /about route to view About Us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  # resources :about, only: [:show]
+  get 'about', to: 'about#show'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -16,6 +19,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
