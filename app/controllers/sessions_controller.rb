de28e4d9e555bcf2e77e166b3ca8b5526ed5df80
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-14 10:41:56 -0500

    added logic to create and destroy methods in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # If the user exists and the password is correct.
+    if user && user.authenticate(params[:password])
+      # save the user id inside the browser cookie. This ios how we keep the user
+      # logged in when they navigate around our webiste.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # If user's login deosn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to 'login'
   end
 
 end
