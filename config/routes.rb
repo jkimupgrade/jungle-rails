669ab286190f9f8c1b0fa10213a670602a0b3f06
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-14 11:05:44 -0500

    fixed login and signup errors and tested functionality

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
-  get '/users' => 'user#create'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
