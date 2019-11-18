9984a2ffeb33cec64fcab4074da978fe6bdd1550
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-18 18:28:38 -0500

    added password minimum length validation and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-
-  validates_presence_of :first_name, :last_name, :password, :password_confirmation
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe User, type: :model do
   
     it 'is valid with valid attributes' do 
       @user = User.create(first_name: 'Jim', last_name: 'Kim', 
-        email: 'test@test.com', password: 'abc123', password_confirmation: 'abc123')
+        email: 'test@test.com', password: 'abcd1234', password_confirmation: 'abcd1234')
       expect(@user.errors.full_messages).to be_empty
     end
     
     it 'is not valid without matching passwords' do 
       @user = User.create(first_name: 'Jim', last_name: 'Kim', 
-        email: 'test@test.com', password: 'abc123', password_confirmation: 'bbc125')
+        email: 'test@test.com', password: 'abcd1234', password_confirmation: 'bbcd1245')
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
@@ -24,28 +24,34 @@ RSpec.describe User, type: :model do
 
     it 'is not valid without first name' do 
       @user = User.create(first_name: nil, last_name: 'Kim', 
-        email: 'test@test.com', password: 'abc123', password_confirmation: 'abc123')
+        email: 'test@test.com', password: 'abcd1234', password_confirmation: 'abcd1234')
       expect(@user.errors.full_messages).to include("First name can't be blank")
     end
 
     it 'is not valid without last name' do 
       @user = User.create(first_name: 'Jim', last_name: nil, 
-        email: 'test@test.com', password: 'abc123', password_confirmation: 'abc123')
+        email: 'test@test.com', password: 'abcd1234', password_confirmation: 'abcd1234')
       expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it 'is not valid without email' do 
       @user = User.create(first_name: 'Jim', last_name: 'Kim', 
-        email: nil, password: 'abc123', password_confirmation: 'abc123')
+        email: nil, password: 'abcd1234', password_confirmation: 'abcd1234')
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'is not valid with existing email (not case sensitive)' do 
       @user1 = User.create(first_name: 'Jim', last_name: 'Kim',
-        email: 'test@test.com', password: 'abc123', password_confirmation: 'abc123')
+        email: 'test@test.com', password: 'abcd1234', password_confirmation: 'abcd1234')
       @user2 = User.create(first_name: 'Jim', last_name: 'Kim',
-        email: 'TEST@TEST.com', password: 'abc123', password_confirmation: 'abc123')
+        email: 'TEST@TEST.com', password: 'abcd1234', password_confirmation: 'abcd1234')
       expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'is not valid with password shorter than 8 characters' do 
+      @user = User.create(first_name: 'Jim', last_name: 'Kim', 
+        email: 'test@test.com', password: 'a', password_confirmation: 'a')
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
     end
 
   end
