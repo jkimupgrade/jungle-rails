a8d63406d1636794dd51fe850d87b1b5a7dc3559
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-18 16:57:00 -0500

    added rspec tests for product class

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    
+    it 'is valid with valid attributes' do 
+      @category = Category.new(name: 'Category A')
+      @product = Product.new(name: 'Product A', price: 10000, quantity: 30, category: @category)
+      expect(@product).to be_valid
+    end
+
+    it 'is not valid without product name' do
+      @category = Category.new(name: 'Category A')
+      @product = Product.new(name: nil, price: 10000, quantity: 30, category: @category)
+      expect(@product).to_not be_valid
+    end
+
+    it 'is not valid without product price' do
+      @category = Category.new(name: 'Category A')
+      @product = Product.new(name: 'Product A', quantity: 30, category: @category)
+      expect(@product).to_not be_valid
+    end
+
+    it 'is not valid without product quantity' do
+      @category = Category.new(name: 'Category A')
+      @product = Product.new(name: 'Product A', price: 10000, quantity: nil, category: @category)
+      expect(@product).to_not be_valid
+    end
+
+    it 'is not valid without product category' do
+      @category = Category.new(name: 'Category A')
+      @product = Product.new(name: 'Product A', price: 10000, quantity: 30, category: nil)
+      expect(@product).to_not be_valid
+    end
+
+  end
 end
